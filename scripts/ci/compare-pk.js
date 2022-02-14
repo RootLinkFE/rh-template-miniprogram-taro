7597b2697d12eb47f454a557c716a930bd0205fb
huxiubin <xiubin.hu@rootcloud.com>
2022-02-14 11:07:42 +0800

    chore: yml调整

--- scripts/ci/compare-pk.js
+++ scripts/ci/compare-pk.js
@@ -1,30 +1,34 @@
-'use strict';
+'use strict'
 
-const fs = require('fs');
-const exec = require('child_process').exec;
+const fs = require('fs')
+const exec = require('child_process').exec
 // const execSync = require('child_process').execSync;
-const npmPackageName = process.env.npm_package_name;
-const uri = './.gitlab-ci.yml';
+const npmPackageName = process.env.npm_package_name
+const uri = './.gitlab-ci.yml'
 
 const uuid = () => {
   return `${Math.random()
     .toString(36)
-    .substr(2)}`;
-};
+    .substr(2)}`
+}
 
 // 监听package.json 文件版本更新，然后脚本自动修改NODE_MODULES_VERSION版本号
-exec('git diff --cached --name-only package.json', function(error, stdout, stderr) {
-  const regexStr = `NODE_MODULES_VERSION: '${npmPackageName}-(.*?)'`;
-  const regex = new RegExp(regexStr, 'g');
-  let newData = '';
+exec('git diff --cached --name-only package.json', function(
+  error,
+  stdout,
+  stderr
+) {
+  const regexStr = `NODE_MODULES_VERSION: '${npmPackageName}-(.*?)'`
+  const regex = new RegExp(regexStr, 'g')
+  let newData = ''
   if (error) {
-    console.error('error: ' + error);
-    return;
+    console.error('error: ' + error)
+    return
   }
-  console.log('git diff stdout: ', stdout);
+  console.log('git diff stdout: ', stdout)
   if (!stdout) {
-    console.log('package.json 无修改');
-    return true
+    console.log('package.json 无修改')
+    throw 'package.json 有修改'
   }
-  return false
-});
+  return true
+})
